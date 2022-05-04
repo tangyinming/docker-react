f43964ea68df4253703b9d505d81ddc0660ce287
Tony <ymtangab@gmail.com>
2022-05-05 00:33:15 +0100

    Initial Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders get Edit", () => {
+  render(<App />);
+  const editElement = screen.getByText(/Edit/i);
+  expect(editElement).toBeInTheDocument();
+});
+
+test("renders get Edit 2", () => {
+  render(<App />);
+  const editElement = screen.getByText(/Edit/i);
+  expect(editElement).toBeInTheDocument();
 });
