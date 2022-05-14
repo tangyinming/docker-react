9a5cefcb41cea0735b9200bf0995148abd9bd468
Tony <ymtangab@gmail.com>
2022-05-15 00:55:49 +0100

    Docker port forwarding fix

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ ssh -o StrictHostKeyChecking=no travis@$DIGITAL_OCEAN_IP_ADDRESS << ENDSSH
     docker image prune -a -f
     echo "Tag to be deployed:"
     echo $VERSION
-    docker run --name docker-react -p 3003:3000 -d ymtangab/docker-react:$VERSION
+    docker run --name docker-react -p 3003:80 -d ymtangab/docker-react:$VERSION
 ENDSSH
